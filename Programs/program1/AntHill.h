2d347d933a874e4b6ec06ed49212764a0cdebdb4
mhamza <moazhamza@gmail.com>
2017-02-22 16:15:34 -0500

    Added sample ant log and readme file

--- Programs/program1/AntHill.h
+++ Programs/program1/AntHill.h
@@ -7,33 +7,38 @@
 #include "LinkedList.h"
 
 class AntHill{
-
-    private:
-        int current_ants;
-        List *listOfAnts = new List();
-        int next_id;
-        unsigned int food;
-        int gridSize;
-        unsigned int numAttacks;
-        unsigned int losses;
-    public:
-        AntHill();
-        ~AntHill();
-
-        int addAnt();
-        bool removeAnt(int);
-
-        Ant *getAnt(int);
-        int getCurrentNumAnts(){return current_ants;}
-        unsigned int getFood(){return food;}
-
-        void attack(); 
-        void move();
-        void fightOrFood(std::fstream&);
-        
-        void outputHillStatus(std::fstream&);
-        void printHillInfo();
-        void turn();
+    
+private:
+    int current_ants;
+    List *listOfAnts = new List();
+    int next_id;
+    unsigned int food;
+    int grid_size;
+    unsigned int num_attacks;
+    unsigned int losses;
+    std::fstream logfile;
+    
+public:
+    AntHill();
+    ~AntHill();
+    
+    int addAnt();
+    bool removeAnt(int);
+    
+    Ant *getAnt(int);
+    int getCurrentNumAnts(){return current_ants;}
+    unsigned int getFood(){return food;}
+    
+    int determineNumDefenders();
+    void removeDefendingAnts();
+    
+    void attack(std::fstream&);
+    void move();
+    void fightOrFood(std::fstream&);
+    
+    void outputHillStatus(std::fstream&);
+    void printHillInfo();
+    void turn();
 };
 
-#endif 
+#endif
